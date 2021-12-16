eb2cdd69ffb47c0c443b0613baada325f8241044
Sair Team <copybara-servicebot@google.com>
2021-12-15 16:08:16 -0800

    Integrate LLVM at llvm/llvm-project@d87e6170487c
    Updates LLVM usage to match
    [d87e6170487c](https://github.com/llvm/llvm-project/commit/d87e6170487c)
    
    PiperOrigin-RevId: 416669329

--- transforms/sair_from_linalg.cc
+++ transforms/sair_from_linalg.cc
@@ -15,7 +15,7 @@
 #include "transforms/sair_from_linalg.h"
 
 #include "llvm/ADT/STLExtras.h"
-#include "mlir/Dialect/Linalg/IR/LinalgOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/Dialect/Utils/StructuredOpsUtils.h"
